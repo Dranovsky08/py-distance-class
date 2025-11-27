b450f420dec1bf428df79913412517eb65065098
Dimanchik <karabinprorus@gmail.com>
2025-11-28 00:17:27 +0200

    solution take8

--- app/main.py
+++ app/main.py
@@ -1,47 +1,51 @@
+from __future__ import annotations
+
 from typing import Union
 
-
-Number = Union[int, float]
+Number = Union[int, float, "Distance"]
 
 
 class Distance:
     def __init__(self, km: float) -> None:
-        self.km = float(km)
+        self.km = km
 
     def __str__(self) -> str:
-        return f"Distance: {self.km} kilometers.\n"
+        return f"Distance: {self.km} kilometers."
 
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Union[Number, "Distance"]) -> "Distance":
-        value = self._value(other)
-        return Distance(self.km + value)
+    def _value(self, other: Number) -> float:
+        if isinstance(other, Distance):
+            return other.km
+        return float(other)
 
-    def __iadd__(self, other: Union[Number, "Distance"]) -> "Distance":
+    def __add__(self, other: Number) -> Distance:
+        return Distance(self.km + self._value(other))
+
+    def __iadd__(self, other: Number) -> Distance:
         self.km += self._value(other)
         return self
 
-    def __mul__(self, other: Number) -> "Distance":
+    def __mul__(self, other: Union[int, float]) -> Distance:
         return Distance(self.km * float(other))
 
-    def __truediv__(self, other: Number) -> "Distance":
+    def __truediv__(self, other: Union[int, float]) -> Distance:
+        if float(other) == 0:
+            raise ZeroDivisionError("Distance cannot be divided by zero.")
         return Distance(round(self.km / float(other), 2))
 
-    def _value(self, other: Union[Number, "Distance"]) -> float:
-        return other.km if isinstance(other, Distance) else float(other)
-
-    def __lt__(self, other: Union[Number, "Distance"]) -> bool:
+    def __lt__(self, other: Number) -> bool:
         return self.km < self._value(other)
 
-    def __gt__(self, other: Union[Number, "Distance"]) -> bool:
+    def __gt__(self, other: Number) -> bool:
         return self.km > self._value(other)
 
-    def __eq__(self, other: Union[Number, "Distance"]) -> bool:
+    def __eq__(self, other: Number) -> bool:
         return self.km == self._value(other)
 
-    def __le__(self, other: Union[Number, "Distance"]) -> bool:
+    def __le__(self, other: Number) -> bool:
         return self.km <= self._value(other)
 
-    def __ge__(self, other: Union[Number, "Distance"]) -> bool:
+    def __ge__(self, other: Number) -> bool:
         return self.km >= self._value(other)
